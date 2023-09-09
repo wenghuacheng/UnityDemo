5808654915328689f6c874021346c530eb79fd81
wenghuacheng <593103283@qq.com>
2023-09-09 12:26:46 +0800

    更新

--- Assets/Demo/UI示例/14.悬浮框/Tooltip.cs
+++ Assets/Demo/UI示例/14.悬浮框/Tooltip.cs
@@ -1,17 +1,26 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 namespace Demo.UI
 {
-    public class Tooltip : MonoBehaviour//, IPointerEnterHandler, IPointerExitHandler
+    public class Tooltip : MonoBehaviour
     {
         [SerializeField] private GameObject tooltip;
+        [SerializeField] private List<TextMeshPro> texts;
 
         private void Awake()
         {
             tooltip.SetActive(false);
+
+            for (int i = 0; i < texts.Count; i++)
+            {
+                texts[i].text = $"AAA{i}:BBB{i}";
+            }
         }
 
         /**
@@ -20,11 +29,47 @@ namespace Demo.UI
         private void OnMouseEnter()
         {
             tooltip.SetActive(true);
+            RefreshText();
         }
 
         private void OnMouseExit()
         {
             tooltip.SetActive(false);
+        }
+
+        /// <summary>
+        /// 让文本从左向右移动
+        /// </summary>
+        private void RefreshText()
+        {
+            for (int i = 0; i < texts.Count; i++)
+            {
+                var text = texts[i];
+                var rect = text.GetComponent<RectTransform>();
+
+                //从左向右的效果
+                StartCoroutine(LeftToRightMoveText(rect, -30, i * 0.2f));
+            }
+        }
+
+        /// <summary>
+        /// 从左到右进入
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="delay"></param>
+        /// <returns></returns>
+        private IEnumerator LeftToRightMoveText(RectTransform transform, int offest, float delay)
+        {
+            var p = transform.position;
+            transform.position = new Vector3(offest, p.y, p.z);
+
+            yield return new WaitForSeconds(delay);
+            transform.DOLocalMoveX(0, 0.3f);
+        }
+
+        private IEnumerator FadeText(RectTransform transform, int offest, float delay)
+        {
+
         }
     }
 }
