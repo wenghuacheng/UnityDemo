73028e16ecefee06bb3a798887a64f907940b78e
wenghuacheng <593103283@qq.com>
2024-02-20 16:50:58 +0800

    更新

---
+++ Assets/Demo/9.编辑器/100.属性编辑器扩展/1.获取参数/TestManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Demo.Editors.PropertiesEditors.GetParameter
+{
+    public class TestManager : MonoBehaviour
+    {
+        [SerializeField] public TestInfo[] testInfos;
+    }
+}
---
+++ Assets/Demo/9.编辑器/100.属性编辑器扩展/2.显示操作按钮/LayoutButtonDisplayItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Demo.Editors.PropertiesEditors.LayoutButton
+{
+    [System.Serializable]
+    public class LayoutButtonDisplayItem
+    {
+        [SerializeField] public string Name;
+        [SerializeField] public int Health;
+        [SerializeField] public int Mana;
+    }
+}
